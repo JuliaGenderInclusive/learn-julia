a30668c5555192339344cc9b8f53314bcf35cecb
Julia Müller <julia.mueller@anglistik.uni-freiburg.de>
2022-03-31 15:47:58 +0200

    change script materials

--- 220331_FirstCode/fix_this_script.jl
+++ 220331_FirstCode/fix_this_script.jl
@@ -5,8 +5,18 @@
 num_1 = 2
 num_2 = "9"
 
+print(num_1, " has the type ", type_of(num_1))
+
 1stCalculation = num_1 - num_2
 
-printline("The result is: " + 1stCalculation)
+printline("The result is: ", 1stCalculation)
+
+
+myCat = "Flocke"
+
+introduction = "My cat's name is "
+
+print(introduction + myCat)
 
 print(Done!)
+
--- 220331_FirstCode/script_1.jl
+++ 220331_FirstCode/script_1.jl
@@ -1,6 +1,9 @@
 #### Working in scripts
 
 # Pluto notebooks are great when we have lots of text and not as much code, but you'll often see scripts in an IDE (integrated developer environment, such as VS Code) instead
+
+# When you start a new script (File -> New file), select the Julia language. This will automatically save the file with the right ending .jl
+
 # Let's take another look at variables in this context
 
 # Assign:
@@ -16,42 +19,40 @@ println(result)
 
 # Alternatively, hit Ctrl + Enter to run the line you're in
 
-# Extensions are helpful when writing scripts. 
-#Hover over the variables to be reminded of their assignments
-
-# When you start a new script (File -> New file), select the Julia language. This will automatically save the file with the right ending .jl
+# Hover over the variables to be reminded of their assignments
 
 
 #### Printing
 
 # The two printing methods in Julia are print() and println()
 
-print("Hello, everyone!")
+#= print("Hello, everyone!")
 print("Hi!")
 
 println("Hey =)")
-println("How's it going?")
+println("How's it going?") =#
 
 # The difference between those two is that println() will add a new line after the output, and line() won't.
 
 # Print several statements by listing them, comma-separated:
 
-println("The result is: ", result)
+#println("The result is: ", result)
 
 # Add line breaks with \n:
 
-println("Line1\nLine2")
+#println("Line1\nLine2")
+
+#println("Line1", "\n", "Line2")
 
 
 #### When things go wrong
 
 # In scripts, the order of assignments matters
 
-println(var2 + var3)
+#println(var2 + var3)
 
-var3 = 10
+#var3 = 10
 
 # We get a UndefVarError - var3 is not defined when it is used
-# The script runs up to that point and then fails.
 # Have a close look at the error message - it tells you in which line the issue is and lets you jump to it to correct the error
 
