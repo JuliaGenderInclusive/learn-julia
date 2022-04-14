4fb1150ee05ee2f42ae520c763cc95af73838382
Julia Müller <julia.mueller@anglistik.uni-freiburg.de>
2022-04-14 19:19:07 +0200

    Create madlibs.jl

---
+++ 220414_StringsContainers/madlibs.jl
@@ -0,0 +1,13 @@
+# Mad libs generator
+
+println("Enter an adjective (= descriptive word):")
+adj = readline()
+
+println("Enter a place:")
+place = readline()
+
+println("Enter a noun (a person or a thing):")
+noun = readline()
+
+sentence = "Last night, I dreamed I was a $adj bird and flew to $place with $noun."
+print(sentence)
